a1ddb1543988f6f88a5b8ae1de5f0be6999e44d8
YagiRyu <ryutaro822@icloud.com>
2022-05-24 21:35:05 +0900

    add function

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,27 +6,89 @@
 /*   By: r-yagi <r-yagi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/10 11:49:48 by r-yagi            #+#    #+#             */
-/*   Updated: 2022/05/22 22:26:17 by r-yagi           ###   ########.fr       */
+/*   Updated: 2022/05/24 21:20:09 by r-yagi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-// void	*ft_calloc(size_t count, size_t size)
-// {
-// 	char	*p;
-// 	size_t	i;
+static size_t	ft_strlen(const char *s)
+{
+	size_t	i;
 
-// 	i = 0;
-// 	p = malloc(count * size);
-// 	if (!p)
-// 	{
-// 		errno = ENOMEM;
-// 		return (NULL);
-// 	}
-// 	while (i < count * size)
-// 	{
-// 		p[i] = '\0';
-// 		i++;
-// 	}
-// }
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+static char	*ft_strcat(char *str, char const *s1, char const *s2)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+		str[i++] = s2[j++];
+	str[i] = '\0';
+	return (str);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	size_t	s1_len;
+	size_t	s2_len;
+
+	if (!s1 || !s2)
+		return (NULL);
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	str = malloc(sizeof(char) * (s1_len + s2_len + 1));
+	if (!(str))
+		return (NULL);
+	str = ft_strcat(str, s1, s2);
+	return (str);
+}
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	char	*p;
+	size_t	i;
+
+	i = 0;
+	p = malloc(count * size);
+	if (!p)
+	{
+		errno = ENOMEM;
+		return (NULL);
+	}
+	while (i < count * size)
+	{
+		p[i] = '\0';
+		i++;
+	}
+	return (p);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	char	*str;
+	size_t	i;
+
+	i = 0;
+	str = (char *)s;
+	while (str[i] != (char) c)
+	{
+		if (str[i] == '\0')
+			return (NULL);
+		i++;
+	}
+	return (str);
+}
